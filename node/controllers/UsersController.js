7b2fc4e4e04381abbed7171a01145e3a3ce710ab
Dani <danielzmansilla@gmail.com>
2023-07-03 09:41:21 +0200

    registro y login ok

--- node/controllers/UsersController.js
+++ node/controllers/UsersController.js
@@ -82,28 +82,105 @@
 
 
 
+// import User from '../models/UsersModel.js';
+// import jwt from 'jsonwebtoken';
+// import bcrypt from 'bcrypt';//NPM I bcrypt
+
+
+// // R E G I S T R O
+// const registerUser = async (req, res) => {
+//     try {
+//         const { name, password, email } = req.body;
+//         const existingUser = await User.findOne({ name });
+//         if (existingUser) {
+//             return res.status(400).json({ message: 'El usuario ya existe' });
+//         }           //Verifica que el usuario existe 
+
+
+
+//         //Crear un nuevo usuario
+//         const newUser = new User({ name, password, email });
+//         const saltRounds = 10;
+//         const hashedPassword = await bcrypt.hash(newUser.password, saltRounds);
+//         newUser.password = hashedPassword;
+
+//         const userCreate = await newUser.save();
+//         console.log(userCreate);
+//         res.status(201).json({ message: 'Usuario registrado correctamente' });
+//     } catch (error) {
+//         console.error('Error al guardar el usuario:', error);
+//         res.status(500).json({ message: 'Error al registrar el usuario' });
+//     }
+// };
+
+
+
+// // L O G I N
+// const loginUser = async (req, res) => {
+//     try {
+//         const { name, password } = req.body;
+//         const existingUser = await User.findOne({ name });
+//         if (!existingUser) {
+//             return res.status(400).json({ message: 'El usuario no existe' });  //Verifica si el usuario existe 
+//         }
+
+//         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);//Verifica  si la constraseña es correcta
+//         if (!isPasswordCorrect) {
+//             return res.status(400).json({ message: 'Contraseña incorrecta' });
+//         }
+
+//         const token = jwt.sign(
+//             { userId: existingUser._id, username: existingUser.name },
+//             'secreto_del_token' // Genera el token de autenticación
+//         );
+
+
+//         res.status(200).json({ token });
+//     } catch (error) {
+//         res.status(500).json({ message: 'Error al iniciar sesión' });
+//     }
+// };
+
+// export { registerUser, loginUser };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import User from '../models/UsersModel.js';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';//NPM I bcrypt
+import bcrypt from 'bcrypt';
 
+const PWD_REGEX = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,60}$/;
 
-// R E G I S T R O
 const registerUser = async (req, res) => {
     try {
-        const { name, password, email } = req.body;
+        const { name, email, password } = req.body;
         const existingUser = await User.findOne({ name });
         if (existingUser) {
             return res.status(400).json({ message: 'El usuario ya existe' });
-        }           //Verifica que el usuario existe 
+        }
 
+        // Validar la contraseña usando la expresión regular definida en el esquema
+        if (!PWD_REGEX.test(password)) {
+            return res.status(400).json({
+                message: 'La contraseña debe contener al menos una letra mayúscula, un número y un carácter especial, y tener entre 8 y 24 caracteres.'
+            });
+        }
 
-
-        //Crear un nuevo usuario
-        const newUser = new User({ name, password, email });
         const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(newUser.password, saltRounds);
-        newUser.password = hashedPassword;
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
+        const newUser = new User({ name, email, password: hashedPassword });
         const userCreate = await newUser.save();
         console.log(userCreate);
         res.status(201).json({ message: 'Usuario registrado correctamente' });
@@ -113,27 +190,23 @@ const registerUser = async (req, res) => {
     }
 };
 
-
-
-// L O G I N
 const loginUser = async (req, res) => {
     try {
         const { name, password } = req.body;
         const existingUser = await User.findOne({ name });
         if (!existingUser) {
-            return res.status(400).json({ message: 'El usuario no existe' });  //Verifica si el usuario existe 
+            return res.status(400).json({ message: 'El usuario no existe' });
         }
 
-        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);//Verifica  si la constraseña es correcta
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if (!isPasswordCorrect) {
             return res.status(400).json({ message: 'Contraseña incorrecta' });
         }
 
         const token = jwt.sign(
             { userId: existingUser._id, username: existingUser.name },
-            'secreto_del_token' // Genera el token de autenticación
+            'secreto_del_token'
         );
-
 
         res.status(200).json({ token });
     } catch (error) {
@@ -142,7 +215,3 @@ const loginUser = async (req, res) => {
 };
 
 export { registerUser, loginUser };
-
-
-
-
--- node/models/UsersModel.js
+++ node/models/UsersModel.js
@@ -20,57 +20,91 @@
 
 
 
+// import mongoose from "mongoose";
+// const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
+// const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+// // const passwordValidator = (value) => {
+// //     return /^(?=.*[a-zA-Z])(?=.*\d).+$/.test(value);
+// // };
+
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: USER_REGEX,
+//       message: 'El nombre debe contener entre 3  y 23 carácteres, con al menos una letra mayúscula'
+//     }
+//   },
+ 
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 5,
+//     validate: {
+//       validator: PWD_REGEX,
+//       message: 'La contraseña debe contener entre 8 y 24 carácteres, con al menos una letra y un número'
+//     }
+//   },
+//   email: {
+//     type: String,
+//     required: true
+   
+//   },
+//   role: {
+//     type: String,
+//     default: 'user' // Rol por defecto para un nuevo usuario
+//   }
+
+
+// });
+
+// const User=mongoose.model('User',userSchema)
+// export default User
+
+
+
+
+
+
+
 import mongoose from "mongoose";
-const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
-// const passwordValidator = (value) => {
-//     return /^(?=.*[a-zA-Z])(?=.*\d).+$/.test(value);
-// };
+
+const USER_REGEX = /^[a-zA-Z]+$/;
+const PWD_REGEX = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,60}$/;
+
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     validate: {
-      validator: USER_REGEX,
-      message: 'El nombre debe contener entre 3  y 23 carácteres, con al menos una letra mayúscula'
+      validator: (value) => USER_REGEX.test(value),
+      message: 'El nombre solo puede contener letras, tanto minúsculas como mayúsculas.'
     }
   },
- 
   password: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 8,
+    maxlength: 240,
     validate: {
-      validator: PWD_REGEX,
-      message: 'La contraseña debe contener entre 8 y 24 carácteres, con al menos una letra y un número'
+      validator: (value) => PWD_REGEX.test(value),
+      message: 'La contraseña debe contener al menos una letra mayúscula, un número y un carácter especial, y tener entre 8 y 24 caracteres.'
     }
   },
   email: {
     type: String,
     required: true
-   
   },
   role: {
     type: String,
-    default: 'user' // Rol por defecto para un nuevo usuario
+    default: 'user'
   }
-
-
 });
 
-const User=mongoose.model('User',userSchema)
-export default User
-
-
-
-
-
-
-
-
-
-
+const User = mongoose.model('User', userSchema);
+export default User;
 
 
 

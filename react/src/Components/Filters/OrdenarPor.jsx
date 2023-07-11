2f59b9fc73ce5aed5cbaff951c4e2b9acaf04443
Dani <danielzmansilla@gmail.com>
2023-07-11 12:55:47 +0200

    InterfazJugadoresArreglada

---
+++ react/src/Components/Filters/OrdenarPor.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './OrdenarPor.css';
+
+export function OrdenarPor({ onOrdenarPor }) {
+  const [orden, setOrden] = useState('');
+
+  const handleOrdenChange = (event) => {
+    const nuevoOrden = event.target.value;
+    setOrden(nuevoOrden);
+    onOrdenarPor(nuevoOrden);
+  };
+
+  return (
+    <div className="ordenar-por">
+      <label htmlFor="orden-select" className="label">Ordenar por:</label>
+      <select id="orden-select" className="select" value={orden} onChange={handleOrdenChange}>
+        <option value="">Ninguno</option>
+        <option value="posicion">Posición</option>
+        <option value="media">Media</option>
+      </select>
+    </div>
+  );
+}
--- react/src/Pages/InterfazJugadores/InterfazJugadores.jsx
+++ react/src/Pages/InterfazJugadores/InterfazJugadores.jsx
@@ -5,20 +5,27 @@ import { Footer } from '../../Components/Footer/Footer';
 import { Banner } from '../../Components/Banner/Banner';
 import './InterfazJugadores.css'
 import { Filters } from '../../Components/Filters/Filters';
-
+import { OrdenarPor } from '../../Components/Filters/OrdenarPor';
 
 export function InterfazJugadores() {
+  const handleOrdenarPor = (opcion) => {
+    // Lógica para ordenar los jugadores según la opción seleccionada
+    console.log(`Ordenar por ${opcion}`);
+  };
   return (
-    <div className='interfaz-jugadores-container'>
-      <Header />
+    <>
+    <Header />
+    <div className='interfaz-jugadores-container footer-container'>      
       <Banner />
       <div className='main-filters-cards'>
         <div className='main-filters'><Filters/></div>
+        <div className='vertical-line'></div> {/* División vertical */}
         <div className='main-cards'><Cards/></div>
+        <div className='main-ordenar'><OrdenarPor onOrdenarPor={handleOrdenarPor} /> {/* Pasa la función onOrdenarPor como prop */}</div>
       </div>
-   
-      <Footer/>
-    </div>
+   </div>
+   <Footer className="footer"/>
+   </>
   );
 }
 

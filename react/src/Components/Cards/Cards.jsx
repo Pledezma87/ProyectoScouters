064a1b3560342578867ae30dcb1cbbd0ef2068e0
Dani <danielzmansilla@gmail.com>
2023-07-27 18:01:49 +0200

    Error ToFixed Arreglado

--- react/src/Components/Cards/Cards.jsx
+++ react/src/Components/Cards/Cards.jsx
@@ -1,3 +1,97 @@
+// import React, { useContext } from 'react';
+// import './Card.css';
+// import { Link } from 'react-router-dom';
+// import { PlayersContext } from '../../Context/Context';
+
+// export function Cards() {
+//   const { data, getAbreviacionPosicion } = useContext(PlayersContext);
+
+//   const url = 'http://localhost:8000/players/avatar';
+
+//   // Verificar si data existe y es un array antes de llamar a map
+//   if (!Array.isArray(data) || data.length === 0) {
+//     return <div>No hay datos disponibles.</div>;
+//   }
+
+//   return (
+//     <div className="container-card-main">
+//       {data.map((player) => {
+//         // Verificar si existen datos de Skills Principales, Tácticas y Físicas
+//         const skillsPrincipales = player.PMetrics[0]?.SkillsPrincipales[0]?.mediaGlobal;
+//         const skillsTacticas = player.PMetrics[0]?.SkillsTacticas[0]?.mediaGlobal;
+//         const skillsFisicas = player.PMetrics[0]?.SkillsFisicas[0]?.mediaGlobal;
+
+//         // // Generar la URL dinámica para la ficha técnica del jugador
+//         // const fichaTecnicaURL = `/FichaJugador/${player.jugador.id}`;
+
+//         return (
+//           <div className="card" key={player.jugador.id}>
+//             <div className="card-main">
+//               <div className="card-header">
+//                 <div className="card-header-bg"></div>
+//                 <img className="card-image" src={`${url}/${player.jugador.Avatar}`} alt="" />
+//               </div>
+//               <h1 className="card-name">
+//                 {player.jugador.Nombre} <span>{player.jugador.Apellidos}</span>
+//               </h1>
+//               <div className="card-attributes">
+//                 <div className="card-attribute">
+//                   <span className="card-attribute__value">{player.jugador.Edad}</span>
+//                   <span className="card-attribute__name">Edad</span>
+//                 </div>
+//                 <div className="card-attribute">
+//                   <span className="card-attribute__value">{player.jugador.Estatura}</span>
+//                   <span className="card-attribute__name">Estatura(m)</span>
+//                 </div>
+//                 <div className="card-attribute">
+//                   <span className="card-attribute__value">{player.jugador.Posición}</span>
+//                   <span className="card-attribute__name">Posición</span>
+//                 </div>
+//                 <div className="card-attribute">
+//                   <span className="card-attribute__value">{skillsPrincipales || '-'}</span>
+//                   <span className="card-attribute__name">Skills Principales</span>
+//                 </div>
+//                 <div className="card-attribute">
+//                   <span className="card-attribute__value">{skillsTacticas || '-'}</span>
+//                   <span className="card-attribute__name">Skills Tácticas</span>
+//                 </div>
+//                 <div className="card-attribute">
+//                   <span className="card-attribute__value">{skillsFisicas || '-'}</span>
+//                   <span className="card-attribute__name">Skills Físicas</span>
+//                 </div>
+//               </div>
+
+//               <div className="media-global">
+//                 <span className="globalmedia-name">RATING</span>
+//                 <span className="globalmedia-number">{player.jugador.Rating}</span>
+//               </div>
+
+//               <div className="button-container">
+//               <Link  key={player.jugador._id} to={`/FichaJugador/${player.jugador._id}`}>
+//                   <button className="rompedor-button">Ficha Técnica</button>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 import React, { useContext } from 'react';
 import './Card.css';
@@ -21,6 +115,12 @@ export function Cards() {
         const skillsPrincipales = player.PMetrics[0]?.SkillsPrincipales[0]?.mediaGlobal;
         const skillsTacticas = player.PMetrics[0]?.SkillsTacticas[0]?.mediaGlobal;
         const skillsFisicas = player.PMetrics[0]?.SkillsFisicas[0]?.mediaGlobal;
+
+        // Redondear los valores de las habilidades y el rating a 1 decimal
+        const skillsPrincipalesRounded = skillsPrincipales ? skillsPrincipales.toFixed(1) : '-';
+        const skillsTacticasRounded = skillsTacticas ? skillsTacticas.toFixed(1) : '-';
+        const skillsFisicasRounded = skillsFisicas ? skillsFisicas.toFixed(1) : '-';
+        const ratingRounded = player.jugador.Rating ? player.jugador.Rating.toFixed(1) : '-';
 
         // // Generar la URL dinámica para la ficha técnica del jugador
         // const fichaTecnicaURL = `/FichaJugador/${player.jugador.id}`;
@@ -49,22 +149,22 @@ export function Cards() {
                   <span className="card-attribute__name">Posición</span>
                 </div>
                 <div className="card-attribute">
-                  <span className="card-attribute__value">{skillsPrincipales.toFixed(1) || '-'}</span>
+                  <span className="card-attribute__value">{skillsPrincipalesRounded}</span>
                   <span className="card-attribute__name">Skills Principales</span>
                 </div>
                 <div className="card-attribute">
-                  <span className="card-attribute__value">{skillsTacticas.toFixed(1) || '-'}</span>
+                  <span className="card-attribute__value">{skillsTacticasRounded}</span>
                   <span className="card-attribute__name">Skills Tácticas</span>
                 </div>
                 <div className="card-attribute">
-                  <span className="card-attribute__value">{skillsFisicas.toFixed(1) || '-'}</span>
+                  <span className="card-attribute__value">{skillsFisicasRounded}</span>
                   <span className="card-attribute__name">Skills Físicas</span>
                 </div>
               </div>
 
               <div className="media-global">
                 <span className="globalmedia-name">RATING</span>
-                <span className="globalmedia-number">{player.jugador.Rating.toFixed(1)}</span>
+                <span className="globalmedia-number">{ratingRounded}</span>
               </div>
 
               <div className="button-container">
@@ -79,21 +179,3 @@ export function Cards() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- react/src/Components/Cards/InformCards.jsx
+++ react/src/Components/Cards/InformCards.jsx
@@ -1,16 +1,97 @@
-import React from 'react';
+// import React from 'react';
+// import './InformCard.css';
+// import { Link } from 'react-router-dom';
+// import jugadorImg from './imgs/InformeIcono.jpg';
+// import { useContext } from 'react';
+// import { InformsContext } from '../../Context/Context';
+// import { PlayersContext } from '../../Context/Context';
+
+
+
+// export function InformCards() {
+//   const { informsData } = useContext(InformsContext);
+//   const { data, getAbreviacionPosicion } = useContext(PlayersContext)
+//   const backgroundImageUrl = jugadorImg;
+
+//   // Verificar si data existe y es un array antes de llamar a map
+//   if (!Array.isArray(data) || data.length === 0) {
+//     return <div>No hay datos disponibles.</div>;
+//   }
+
+//   return (
+//     <div className="nuevocontainer">
+//       {informsData.map((informs) => {
+//         console.log("informsData:", informsData);
+//         console.log("informs:", informs);
+
+
+//         // Obtener la fecha en formato de objeto Date
+//         const fechaInforme = new Date(informs.Created_At);
+
+//         // Obtener el día, mes y año de la fecha
+//         const dia = String(fechaInforme.getDate()).padStart(2, '0');
+//         const mes = String(fechaInforme.getMonth() + 1).padStart(2, '0'); // Sumamos 1 al mes, ya que en JavaScript los meses son indexados desde 0 (enero es 0, febrero es 1, etc.)
+//         const año = String(fechaInforme.getFullYear()).slice(-2); // Obtenemos los últimos dos dígitos del año 
+
+//         const player = data.find((playerData) => playerData.jugador._id === informs.PlayerId);
+//         console.log("player:", player);
+
+//         // Obtener la posición abreviada
+//         const posicionAbreviada = getAbreviacionPosicion(player ? player.jugador.Posición : 'Posición Desconocida');
+
+//         return (
+//           <div className="nuevocard" key={informs.id}>
+//             <div className="nuevocard-main">
+//               <div className="nuevocard-header">
+//                 <div className="nuevocard-header-bg"></div>
+//                 <img className="nuevocard-image" src={backgroundImageUrl} alt="" />
+//               </div>
+//               <h1 className="nuevocard-name"> {player ? `${player.jugador.Nombre} ${player.jugador.Apellidos}` : 'Nombre del Jugador Desconocido'}
+//                 <span>({player ? `${posicionAbreviada}` : 'Posición Desconocida'})</span></h1>
+
+//               <div className='fecha-informe'>
+//                 <p>Fecha del Informe <span>{`${dia}/${mes}/${año}`}</span></p>
+//                 <p>{informs.Texto}</p>
+//               </div>
+
+//               <div className='divider'></div>
+
+//               <div className='info-container'>
+//                 <p className='info-column'>Ult. Eval. <span>{informs.MediaInforme}</span></p>
+//                 <p className='info-column'>Informes <span>6</span></p>
+//                 <p className='info-column'>Prox.Seguim. <span>05/10/23</span></p>
+//               </div>
+
+//               <div className="nuevobutton-container">
+//                 <Link to="#">
+//                   <button className="nuevorompedor-button">Leer Más</button>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+import React, { useContext } from 'react';
 import './InformCard.css';
 import { Link } from 'react-router-dom';
 import jugadorImg from './imgs/InformeIcono.jpg';
-import { useContext } from 'react';
 import { InformsContext } from '../../Context/Context';
 import { PlayersContext } from '../../Context/Context';
 
-
-
 export function InformCards() {
   const { informsData } = useContext(InformsContext);
-  const { data, getAbreviacionPosicion } = useContext(PlayersContext)
+  const { data, getAbreviacionPosicion } = useContext(PlayersContext);
   const backgroundImageUrl = jugadorImg;
 
   // Verificar si data existe y es un array antes de llamar a map
@@ -23,7 +104,6 @@ export function InformCards() {
       {informsData.map((informs) => {
         console.log("informsData:", informsData);
         console.log("informs:", informs);
-
 
         // Obtener la fecha en formato de objeto Date
         const fechaInforme = new Date(informs.Created_At);
@@ -38,6 +118,9 @@ export function InformCards() {
 
         // Obtener la posición abreviada
         const posicionAbreviada = getAbreviacionPosicion(player ? player.jugador.Posición : 'Posición Desconocida');
+
+        // Verificar si informs.MediaInforme tiene un valor numérico o asignar 0 si es undefined
+        const informMediaInforme = typeof informs.MediaInforme === 'number' ? informs.MediaInforme : 0;
 
         return (
           <div className="nuevocard" key={informs.id}>
@@ -57,7 +140,7 @@ export function InformCards() {
               <div className='divider'></div>
 
               <div className='info-container'>
-                <p className='info-column'>Ult. Eval. <span>{informs.MediaInforme.toFixed(1)}</span></p>
+                <p className='info-column'>Ult. Eval. <span>{informMediaInforme.toFixed(1)}</span></p>
                 <p className='info-column'>Informes <span>6</span></p>
                 <p className='info-column'>Prox.Seguim. <span>05/10/23</span></p>
               </div>
@@ -74,11 +157,3 @@ export function InformCards() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
